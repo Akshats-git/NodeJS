3f5fb0d6f15d65d7d709cf32ade5a096b86c3203
Akshat <akshatgupta099@gmail.com>
2026-07-18 13:06:46 +0530

    add file operations examples and create initial test and names files

--- Code/file.js
+++ Code/file.js
@@ -1,1 +1,32 @@
 const fs=require("fs")
+
+// Sync
+fs.writeFileSync("./test.txt","Hello World")
+
+// Async
+fs.writeFileSync("./test.txt","Hello World",(err)=>{})
+
+const result = fs.readFileSync("./names.txt","utf-8")
+console.log(result)
+
+fs.readFile("./names.txt","utf-8",(err,data)=>{
+    if(err)
+        console.log("Some error occured: ",err)
+    else 
+        console.log(data)
+})
+
+// used for appending
+fs.appendFileSync("./names.txt",`\nHey! Here at ${Date.now()}`)
+
+// used to make a copy
+fs.cpSync("names.txt","copy.txt")
+
+// used for deleting a file
+fs.unlinkSync("./copy.txt")
+
+// gives various stats
+console.log(fs.statSync("./test.txt"))
+
+
+
