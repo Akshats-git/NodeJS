3b78f373fb3e296d5b3ebbd1d6f7a37c6fcb278e
Akshat <akshatgupta099@gmail.com>
2026-07-19 16:50:15 +0530

    implement HTTP server with request logging and create log file

---
+++ Code/8.handlingURLs.js
@@ -0,0 +1,30 @@
+const http=require('http')
+const fs=require('fs')
+const url=require('url')
+const { listenerCount } = require('cluster')
+
+const server = http.createServer((req,res)=>{
+    if(req.url=="/favicon.ico") return res.end()
+    const log = `${Date.now()}: Request at ${req.url}\n`
+    const myUrl = url.parse(req.url,true)
+    fs.appendFile("log.txt",log,(err)=>{
+        switch(myUrl.pathname){
+            case '/':
+                res.end("Welcome to Home Page")
+                break;
+            case '/about':
+                const username=myUrl.query.username;
+                res.end(`Welcome ${username}`)
+                break
+            case '/search':
+                const search=myUrl.query.search_query;
+                res.end(`Here are the results for ${search}`)
+                break
+            default:
+                res.end("404 Not Found")
+        }
+    })
+})
+
+server.listen(8000,()=>{console.log("Server Started");
+})
